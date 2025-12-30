22a280e05e6b3bbde68321a51b75d0c67c98578b
Jeffreasy <laventejeffrey@gmail.com>
2025-12-30 23:52:48 +0100

    Optimize for Vercel: Add adapter, downgrade Vite, fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@ import { defineConfig } from 'astro/config'
 import { storyblok } from '@storyblok/astro'
 import { loadEnv } from 'vite'
 import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 const env = loadEnv('', process.cwd(), 'STORYBLOK')
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  adapter: vercel(),
   integrations: [
     storyblok({
       accessToken: env.STORYBLOK_TOKEN || 'vgdfUWE4CP2d3ZXnPtoikQtt',
@@ -21,7 +24,6 @@ export default defineConfig({
         page: 'storyblok/structural/Page',
         grid: 'storyblok/structural/Grid',
         columns: 'storyblok/structural/Grid',
-        Columns: 'storyblok/structural/Grid',
         gridmenu: 'storyblok/structural/Gridmenu',
         programmaDag1: 'storyblok/structural/ProgrammaDag',
         programmaDag2: 'storyblok/structural/ProgrammaDag',
